44a2393331f0d1605baafe81d1ce6a5368250ebc
Roman Maiun <roman.maiun@unicorn.com>
2021-06-04 12:34:25 +0300

    improve players info for admins

--- server/arbiter-core/src/main/java/com/arbiter/core/dto/player/PlayerDto.java
+++ server/arbiter-core/src/main/java/com/arbiter/core/dto/player/PlayerDto.java
@@ -2,9 +2,9 @@ package com.arbiter.core.dto.player;
 
 import com.arbiter.core.domain.Player;
 
-public record PlayerDto(String id, String surname, String tid, boolean admin, boolean notificationsEnabled) {
+public record PlayerDto(String id, String surname, String tid, boolean admin, boolean notificationsEnabled, boolean active) {
 
   public static PlayerDto fromPlayer(Player p) {
-    return new PlayerDto(p.getId(), p.getSurname(), p.getTid(), p.isAdmin(), p.isNotificationsEnabled());
+    return new PlayerDto(p.getId(), p.getSurname(), p.getTid(), p.isAdmin(), p.isNotificationsEnabled(), p.isActive());
   }
 }
--- server/arbiter-flows/src/main/java/com/arbiter/flows/processor/ListPlayersCmdProcessor.java
+++ server/arbiter-flows/src/main/java/com/arbiter/flows/processor/ListPlayersCmdProcessor.java
@@ -1,7 +1,9 @@
 package com.arbiter.flows.processor;
 
 
+import com.arbiter.core.domain.Player;
 import com.arbiter.core.dto.player.FoundAllPlayers;
+import com.arbiter.core.dto.player.PlayerDto;
 import com.arbiter.core.service.PlayerService;
 import com.arbiter.flows.dto.BotInputMessage;
 import com.arbiter.flows.dto.OutputMessage;
@@ -23,7 +25,8 @@ public class ListPlayersCmdProcessor implements CommandProcessor {
   @Override
   public OutputMessage process(BotInputMessage input, int msgId) {
     var data = playerService.findAllPlayers();
-    var str = format(data);
+    var moderator = playerService.findPlayerByTid(input.tid());
+    var str = format(data, moderator);
     return OutputMessage.ok(input.chatId(), msgId, str);
   }
 
@@ -32,13 +35,25 @@ public class ListPlayersCmdProcessor implements CommandProcessor {
     return List.of(LIST_PLAYERS_CMD);
   }
 
-  private String format(FoundAllPlayers data) {
+  private String format(FoundAllPlayers data, Player moderator) {
     if (data.players().isEmpty()) {
       return String.format("%sNo active players were found%s", PREFIX, SUFFIX);
     }
     String players = IntStream.range(0, data.players().size())
-        .mapToObj(i -> String.format("%d|%s", i + 1, StringUtils.capitalize(data.players().get(i).surname())))
+        .mapToObj(i -> String.format("%d|%s", i + 1, formatPlayerLine(data.players().get(i), moderator)))
         .collect(Collectors.joining("\n"));
     return String.format("%s%s%s", PREFIX, players, SUFFIX);
+  }
+
+  private String formatPlayerLine(PlayerDto playerDto, Player moderator) {
+    if (moderator.isActive() && moderator.isAdmin()) {
+      var admin = playerDto.admin() ? 1 : 0;
+      var notifications = playerDto.notificationsEnabled() ? 1 : 0;
+      var active = playerDto.active() ? 1 : 0;
+      var player = StringUtils.capitalize(playerDto.surname());
+      return String.format("%s [%d%d%d]", player, admin, notifications, active);
+    } else {
+      return StringUtils.capitalize(playerDto.surname());
+    }
   }
 }
