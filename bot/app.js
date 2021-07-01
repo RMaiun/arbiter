aeca0b9c3a69e601d5e6cc9063f648a976eab286
Roman Maiun <roman.maiun@unicorn.com>
2021-07-01 18:06:37 +0300

    add delay for bot start

--- bot/app.js
+++ bot/app.js
@@ -1,16 +1,25 @@
 const { BotRunner } = require('./app/bot-runner')
 const dotenv = require('dotenv')
 
-console.log('Starting bot...')
+console.log('BOT: Starting...')
 dotenv.config()
+console.log('BOT: Configs were loaded')
+
 const br = new BotRunner(process.env.TOKEN)
-br.initBot().then(bot => {
-  process.once('SIGTERM', () => bot.stop('SIGTERM'))
-  process.once('SIGINT', () => bot.stop('SIGINT'))
-  console.log('Bot was successfully started')
-  return bot.launch()
-}).catch(err => {
-  console.error(err)
-  process.exit(1)
-  console.log('Bot has crashed unexpectedly')
-})
+delay(20000)
+  .then(x => br.initBot())
+  .then(bot => {
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    console.log('BOT: Started successfully')
+    return bot.launch()
+  }).catch(err => {
+    console.error(err)
+    console.log('BOT: unexpected error')
+    process.exit(1)
+  })
+
+function delay (ms) {
+  console.log('BOT: Delay for 20 sec')
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
